17d45366a35c1fd4bf2e60a324d8d76c52b48e79
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-12-11 18:50:51 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+import entities.Product;
+
+public class Program {
+	public static void main(String[] args) {
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+
+		Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+		list.sort(comp);
+
+		for (Product p : list) {
+			System.out.println(p);
+		}
+	}
+}
